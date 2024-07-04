e6f7d0eec5b3a765c367db787a618869106b2abe
Thanaphat Sanngoen <blazesonic14@gmail.com>
2024-07-04 17:28:31 +0700

    commit 7.00

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -8,16 +8,15 @@ import { RouterLink, RouterView } from 'vue-router'
       <div class="wrapper">
         <nav>
           <RouterLink to="/">Home</RouterLink>|
-          <RouterLink to="/About">About</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
         </nav>
-
       </div>
     </header>
+    <RouterView />
   </div>
 </template>
 
 <style scoped>
-
 #layout {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,20 +24,17 @@ import { RouterLink, RouterView } from 'vue-router'
   text-align: center;
   color: #2c3e50;
 }
-  
+
 nav {
-    padding: 30px;
+  padding: 30px;
 }
 
 nav a {
   font-weight: bold;
-  color:#2c3e50;
+  color: #2c3e50;
 }
 
-
-
-nav a.router-link-exact-active{
+nav a.router-link-exact-active {
   color: #42b983;
 }
-
 </style>

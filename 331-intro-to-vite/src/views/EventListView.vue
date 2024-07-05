0625eaf1f6e6f3a6c916b74fd05ad595faa732e7
thanaphat <thanaphat_sanngoen@cmu.ac.th>
2024-07-05 10:16:53 +0700

    commit 15.3 gonna update

---
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import EventDetail from '@/components/EventDetail.vue'
+import Event from '@/types/Event'
+import { ref,onMounted } from 'vue'
+import axios from 'axios'
+
+const events = ref<Event[]>(null)
+
+onMounted(()=>
+axios
+    .get('[https://my-json-server.typicode.com/thanaphat14/Lab2--MockServer/events]')
+    .then((response)=>{
+      console.log(response.data)
+    })
+    .catch((error)=>{
+      console.error('There was an error!,error')
+    }))
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <!-- new element -->
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <EventDetail v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>

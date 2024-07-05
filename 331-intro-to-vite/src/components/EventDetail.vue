147a1c00f0f62a77fe4a20f57f7589fa772d6324
thanaphat <thanaphat_sanngoen@cmu.ac.th>
2024-07-05 09:16:21 +0700

    commit 9.00

--- 331-intro-to-vite/src/components/EventDetail.vue
+++ 331-intro-to-vite/src/components/EventDetail.vue
@@ -6,30 +6,30 @@ defineProps<{
   event: Event
 }>()
 //const event = ref({
-  //  id: 5928101,
-  //  category: 'animal welfare',
-  //  title: 'Cat Adoption Day',
-  //  description: 'Find your new feline friend at this event.',
-  //  location: 'Meow Town',
-  //  date: 'January 28, 2022',
-  //  time: '12:00',
-  //  petsAllowed: true,
-  //  organizer: 'Kat Laydee'
-  //})
+//  id: 5928101,
+//  category: 'animal welfare',
+//  title: 'Cat Adoption Day',
+//  description: 'Find your new feline friend at this event.',
+//  location: 'Meow Town',
+//  date: 'January 28, 2022',
+//  time: '12:00',
+//  petsAllowed: true,
+//  organizer: 'Kat Laydee'
+//})
 </script>
 
 <template>
-<div class="event-class">
-  <div class="event-details">
-    <h2>{{ event.title }}</h2>
+  <div class="event-class">
+    <div class="event-details">
+      <h2>{{ event.title }}</h2>
       <h2>{{ event.category }} on {{ event.organizer }}</h2>
+    </div>
   </div>
-</div>
 </template>
 
 <style scoped>
 .event-details {
-    padding: 20px;
+  padding: 20px;
   width: 250px;
   cursor: pointer;
   border: 1px solid #39495c;
@@ -41,5 +41,6 @@ defineProps<{
 }
 .event-details:hover {
   transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);}
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
 </style>
--- 331-intro-to-vite/src/type.ts
+++ 331-intro-to-vite/src/type.ts
@@ -1,10 +1,10 @@
 export interface Event {
-    id: number
-    category: string
-    title: string
-    location: string
-    date: string
-    time: string
-    petsAllowed: boolean
-    organizer: string
+  id: number
+  category: string
+  title: string
+  location: string
+  date: string
+  time: string
+  petsAllowed: boolean
+  organizer: string
 }
